6e2c235d8b22e4aff18d3d56e51b42c8bb051999
NESCHAL SINGH <143714032+neschal-s@users.noreply.github.com>
2025-04-04 03:01:11 +0530

    Add files via upload

---
+++ index.mjs
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+
+console.log(chalk.blue("Hello World"));
+console.log(chalk.red.bold("This is the testing of node package"));
+
+//
+//node --experimental-modules index.mjs
+//
