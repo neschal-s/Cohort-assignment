5f256af0ab0027390aa36d6df499675ca55d324d
NESCHAL SINGH <143714032+neschal-s@users.noreply.github.com>
2025-04-09 02:55:06 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 const express=require('express');
 const app=express();
 
-app.get("/",function(req,res){
-    res.send("")
-})
 
-app.listen(3000);
+// function isOldEnough(age){
+//     if(age>=14)
+//         return true;
+//     else   
+//         return false;
+// }
+
+function isOldEnoughMiddleware(req,res,next)
+{
+    const age=req.query.age;
+    if(age>=14)
+        next();
+    else
+    {
+        res.status(411).json({
+            msg: "sorry you are undeage."
+        })
+    }
+}
+
+
+app.get("/ride1",isOldEnoughMiddleware,function(req,res){
+    res.json({
+        msg: "You have successfully riden the ride 1"
+    });
+});
+
+
+
+app.get("/ride2",isOldEnoughMiddleware,function(req,res){
+    res.json({
+        msg: "You have successfully riden the ride 2"
+    });
+});
+
+app.listen(3000)
